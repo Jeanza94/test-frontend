1e256020808ce3b74a1e124acbd81b7d434fbdc5
Jean Zapata <jeanza94@hotmail.com>
2024-02-21 20:31:20 -0500

    CustomInput created

---
+++ src/shared/components/Input.tsx
@@ -0,0 +1,17 @@
+
+import { FC, InputHTMLAttributes } from 'react'
+
+interface CustomInputProps extends InputHTMLAttributes<HTMLInputElement>{}
+
+const Input:FC<CustomInputProps>= (props) => {
+  return (
+    <input
+      className='w-full p-2 text-xs tablet:text-sm focus:outline-none' 
+      {
+        ...props
+      }
+    />
+  )
+}
+
+export default Input
