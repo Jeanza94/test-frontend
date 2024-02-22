8e04ec8f6f428c6b424126be4127715aeaef6b01
Jean Zapata <jeanza94@hotmail.com>
2024-02-21 20:30:58 -0500

    tailwind extended theme-screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,8 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,6 +12,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#ffe600",
+        secondary: "#3483fa"
+      },
+      screens: {
+        mobile: '200px',
+        tablet: '600px',
+        desktop: '1024px'
+      }
     },
   },
   plugins: [],
