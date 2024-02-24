134e5df1b1abbd0f41db2aa7de6677f62766ee09
Jean Zapata <jeanza94@hotmail.com>
2024-02-24 08:08:27 -0500

    add domain image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+        "http2.mlstatic.com",
+    ]
+},
+};
 
 export default nextConfig;
